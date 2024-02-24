d97807445a5a2c6c1af9343db0197a8d75076f20
Nisarg Amlani <114862611+Nisarg155@users.noreply.github.com>
2024-02-24 13:32:14 +0530

    added login and sign up with firebase

--- terminal_titans/urls.py
+++ terminal_titans/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from terminal_titans import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login),
 ]
