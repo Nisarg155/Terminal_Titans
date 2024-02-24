9f7954b2ac27301c8bfe1f24722ec524d1cae382
Arsh <arshgangani18@gmail.com>
2024-02-24 12:23:52 +0530

    Added models of hospital and user

--- hospital/models.py
+++ hospital/models.py
@@ -15,3 +15,25 @@ class Doctor(models.Model):
     
 
 # Create your models here.
+class hospital_details(models.Model):
+    hospital_img = models.ImageField(upload_to="hospital/")
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True,null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+
+class User(models.Model):
+    user_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    # email = models.EmailField(max_length=50)
+    blood_group = models.CharField(max_length=5)
+    mobile_no = models.IntegerField(max_length=10)
+    allergies_description = models.TextField(blank=True,null=True)
+
+
+
