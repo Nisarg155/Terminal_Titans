89f9451ad37319209ea038b2f514b6da245426a3
Samarth Ganjawala <samarthganjawala202@gmail.com>
2024-02-24 12:23:40 +0530

    Doctor model

--- hospital/models.py
+++ hospital/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Doctor(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True,max_length=40)
+    specialist=models.CharField(max_length=30)
+    experience_in_year=models.IntegerField()
+    description=models.TextField(blank=True,null=True)
+    
 # Create your models here.
