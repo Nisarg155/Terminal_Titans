3f4a04c8c0e217800d0dafcab0f83c8bf74b8f80
Arsh <arshgangani18@gmail.com>
2024-02-25 00:52:02 +0530

    migration error solved

--- hospital/admin.py
+++ hospital/admin.py
@@ -9,11 +9,3 @@ admin.site.register(hospital_details)
 admin.site.register(Doctor)
 admin.site.register(User)
 
-
-from django.contrib import admin
-from .models import Doctor,hospital_details,User
-# Register your models here.
-
-admin.site.register(Doctor)
-admin.site.register(User)
-admin.site.register(hospital_details)
