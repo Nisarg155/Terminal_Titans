658c99cb853065e17f92bb8c9f0e41079c67de85
Arsh <arshgangani18@gmail.com>
2024-02-24 12:34:24 +0530

    branch work

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Doctor,hospital_details,User
 # Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(User)
+admin.site.register(hospital_details)
